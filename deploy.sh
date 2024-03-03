447d031a5931f16e7808bc4d182701a39d2c433d
Frugan <dev@frugan.it>
2024-03-03 16:13:27 +0100

    feat!: add support to v21-alpine
    shellcheck and lint scripts
    update docs

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,37 @@
 #https://blog.dockbit.com/templating-your-dockerfile-like-a-boss-2a84a67d28e9
 
 deploy() {
-  str="
+	str="
   s!%%TAG%%!$TAG!g;
 "
 
-  sed -r "$str" $1
+	sed -r "$str" "$1"
 }
 
 #https://docs.strapi.io/dev-docs/installation/cli
 # Only Active LTS or Maintenance LTS versions are supported (currently v18 and v20).
 # Odd-number releases of Node, known as "current" versions of Node.js, are not supported (e.g. v19, v21).
 TAGS=(
-  20-alpine
+	20-alpine
+	21-alpine
 )
 
 ENTRYPOINT=entrypoint.sh
 
+IFS='
+'
+# shellcheck disable=SC2048
 for TAG in ${TAGS[*]}; do
 
-  if [ -d "$TAG" ]; then
-    rm -Rf $TAG
-  fi
+	if [ -d "$TAG" ]; then
+		rm -Rf "$TAG"
+	fi
 
-  mkdir $TAG
-  deploy Dockerfile.template > $TAG/Dockerfile
+	mkdir "$TAG"
+	deploy Dockerfile.template >"$TAG"/Dockerfile
 
-  if [ -f "$ENTRYPOINT" ]; then
-    cp $ENTRYPOINT $TAG
-  fi
+	if [ -f "$ENTRYPOINT" ]; then
+		cp $ENTRYPOINT "$TAG"
+	fi
 
 done
